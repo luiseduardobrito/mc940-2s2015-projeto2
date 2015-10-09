d5c393bb699396b837ae816d0f1589b479daa4c1
Luis Eduardo Brito <luiseduardo14@gmail.com>
2015-10-09 18:56:19 -0300

    Build: Run script para execucao dos testes

--- projeto2.m
+++ projeto2.m
@@ -1,21 +1,5 @@
-function fileList = getAllFiles(dirName)
-
-  dirData = dir(dirName);      %# Get the data for the current directory
-  dirIndex = [dirData.isdir];  %# Find the index for directories
-  fileList = {dirData(~dirIndex).name}';  %'# Get a list of the files
-  if ~isempty(fileList)
-    fileList = cellfun(@(x) fullfile(dirName,x),...  %# Prepend path to files
-                       fileList,'UniformOutput',false);
-  end
-  subDirs = {dirData(dirIndex).name};  %# Get a list of the subdirectories
-  validIndex = ~ismember(subDirs,{'.','..'});  %# Find index of subdirectories
-                                               %#   that are not '.' or '..'
-  for iDir = find(validIndex)                  %# Loop over valid subdirectories
-    nextDir = fullfile(dirName,subDirs{iDir});    %# Get the subdirectory path
-    fileList = [fileList; getAllFiles(nextDir)];  %# Recursively call getAllFiles
-  end
-
-end
+#!/usr/local/bin/octave -qf
 
 dir = "input"
-disp(readdir (dir))
+files = readdir (dir)
+imshow(files(0))
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+chmod +x projeto2.m
+./projeto2.m #&> /dev/null;
