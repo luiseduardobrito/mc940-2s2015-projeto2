a3f56d01beb2088f2c0e564ff4bcdbfb17c91965
Luis Eduardo Brito <luiseduardo14@gmail.com>
2015-10-10 21:31:05 -0300

    Feat: Convertendo imagens para escalas de cinza

--- projeto2.m
+++ projeto2.m
@@ -1,13 +1,20 @@
 #!/usr/local/bin/octave -q
 
+pkg load image;
+
 files = readdir ("input");
 frames = [];
+hists = [];
 
 for c = 1 : uint64(size(files)(1))
 
 	if strcmp(char(files{c}), ".") == 0 && strcmp(char(files{c}), "..") == 0
-		frames{c} = imread(strcat("input/", files{c}));
+
+		[frames{c}, map, alpha] = imread(strcat("input/", files{c}));
    		disp(strcat("Loaded frame #", int2str(c)));
+	
+		imwrite(rgb2gray(frames{c}), strcat("output/", files{c}));
+
    	endif
 
 endfor
